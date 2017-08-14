ba36c15da99940da1f7cdc244757b66939b76c73
gab luz <gabi_cluz@hotmail.com>
2017-08-14 09:50:07 -0300

    Commit inicial.

---
+++ gerenciador-pedidos/src/main/java/br/com/gabriel/gerenciadorpedidos/GerenciadorPedidosApplication.java
@@ -0,0 +1,12 @@
+package br.com.gabriel.gerenciadorpedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorPedidosApplication.class, args);
+	}
+}
